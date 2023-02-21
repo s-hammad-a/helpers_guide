e5d8316101d8ed3a13c04f4099358a87379afe3d
syedhammadali <syedhammadali2001@gmail.com>
2023-02-22 02:31:03 +0500

    "21/02/2023"

--- lib/screens/visitor_home_screen.dart
+++ lib/screens/visitor_home_screen.dart
@@ -96,211 +96,8 @@ class VisitorHome extends StatelessWidget {
         physics: const NeverScrollableScrollPhysics(),
         controller: context.watch<HomeScreenProvider>().controller,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                  color: Color(0xFF428DFC),
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
-                ),
-                height: MediaQuery.of(context).size.height/4.5,
-                width: double.maxFinite,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Expanded(child: SizedBox.shrink()),
-                    Text(
-                      " Hi ${user!.displayName == null ? "${user!.displayName}": ""} !",
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    const SizedBox(height: 10,),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                        Text(
-                          " Hafr Al-Batin University",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20,),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10,),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-                child: SizedBox(
-                  height: 25,
-                  child: Row(
-                    children: [
-                      const Text(
-                        "University Information",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      const Expanded(child: SizedBox.shrink()),
-                      user.email!.contains("helper.com") ? IconButton(
-                        splashRadius: 25,
-                        padding: EdgeInsets.zero,
-                        icon: const Icon(
-                          Icons.edit,
-                        ),
-                        onPressed: () {
-                          Navigator.pushNamed(context, "/editUniversity");
-                        },
-                      ) : const SizedBox.shrink(),
-                    ],
-                  ),
-                ),
-              ),
-              const UniversityInformation(),
-            ],
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(20),
-                decoration: const BoxDecoration(
-                  color: Color(0xFF428DFC),
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
-                ),
-                height: MediaQuery.of(context).size.height/4.5,
-                width: double.maxFinite,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Expanded(child: SizedBox.shrink()),
-                    Text(
-                      " Hi ${user!.displayName == null ? "${user!.displayName}": ""} !",
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    const SizedBox(height: 10,),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                          size: 30,
-                        ),
-                        Text(
-                          " Hafr Al-Batin University",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20,),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10,),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Color(0xFFA1C3FC),
-                  ),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: const [
-                        SizedBox(height: 5,),
-                        Text(
-                          "شروط و ضوابط القبول",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 25,
-                              fontWeight: FontWeight.bold
-                          ),
-                          textDirection: TextDirection.rtl,
-                        ),
-                        SizedBox(height: 5,),
-                        Text(
-                          '''- أن يكون الطالب سعودياً أو من أم سعودية.
-- أن يكون الطالب قد حصل على الشهادة الثانوية السعودية أو ما يعادلها في العام الحالي أو الأربع سنوات الماضية  
-- أن يكون الطالب قد تقدم لاختباري القدرات العامة والتحصيلي المقدمين من المركز الوطني للقياس .
-- ألا يكون للطالب سجل حالي أو سابق في جامعة حفر الباطن.''',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18,
-                          ),
-                          textDirection: TextDirection.rtl,
-                        ),
-                        SizedBox(height: 5,),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 10,),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    color: Color(0xFFA1A3FC),
-                  ),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.zero,
-                    title: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        const SizedBox(height: 5,),
-                        const Text(
-                          "رابط موقع الجامعه الرسمي",
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold
-                          ),
-                          textDirection: TextDirection.rtl,
-                        ),
-                        const SizedBox(height: 5,),
-                        Text(
-                          "https://portal.uhb.edu.sa",
-                          style: TextStyle(
-                            color: Colors.blue.shade800,
-                            fontSize: 18,
-                          ),
-                        ),
-                        const SizedBox(height: 5,),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
+          const HomePage2(),
+          const Registration(),
           Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -343,7 +140,7 @@ class VisitorHome extends StatelessWidget {
                     ),
                     Expanded(
                       child: Text(
-                        user.displayName != null ? user.displayName! : " ",
+                        user!.displayName != null ? user.displayName! : " ",
                         style: const TextStyle(
                           color: Colors.black,
                           fontSize: 20,
@@ -436,6 +233,229 @@ class VisitorHome extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class Registration extends StatelessWidget {
+  const Registration({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = Provider.of<User?>(context);
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: const BoxDecoration(
+              color: Color(0xFF428DFC),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+            ),
+            height: MediaQuery.of(context).size.height/4.5,
+            width: double.maxFinite,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Expanded(child: SizedBox.shrink()),
+                Text(
+                  " Hi ${user != null && user.displayName == null ? "${user.displayName}": ""} !",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                const SizedBox(height: 10,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    Text(
+                      " Hafr Al-Batin University",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20,),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Color(0xFFA1C3FC),
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    SizedBox(height: 5,),
+                    Text(
+                      "شروط و ضوابط القبول",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
+                      ),
+                      textDirection: TextDirection.rtl,
+                    ),
+                    SizedBox(height: 5,),
+                    Text(
+                      '''- أن يكون الطالب سعودياً أو من أم سعودية.
+- أن يكون الطالب قد حصل على الشهادة الثانوية السعودية أو ما يعادلها في العام الحالي أو الأربع سنوات الماضية  
+- أن يكون الطالب قد تقدم لاختباري القدرات العامة والتحصيلي المقدمين من المركز الوطني للقياس .
+- ألا يكون للطالب سجل حالي أو سابق في جامعة حفر الباطن.''',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                      textDirection: TextDirection.rtl,
+                    ),
+                    SizedBox(height: 5,),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10,),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Color(0xFFA1A3FC),
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(height: 5,),
+                    const Text(
+                      "رابط موقع الجامعه الرسمي",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                      ),
+                      textDirection: TextDirection.rtl,
+                    ),
+                    const SizedBox(height: 5,),
+                    Text(
+                      "https://portal.uhb.edu.sa",
+                      style: TextStyle(
+                        color: Colors.blue.shade800,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 5,),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+  }
+}
+
+class HomePage2 extends StatelessWidget {
+  const HomePage2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = Provider.of<User?>(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            color: Color(0xFF428DFC),
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
+          ),
+          height: MediaQuery.of(context).size.height/4.5,
+          width: double.maxFinite,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Expanded(child: SizedBox.shrink()),
+              Text(
+                " Hi ${user != null && user.displayName == null ? "${user.displayName}": ""} !",
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 10,),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: const [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  Text(
+                    " Hafr Al-Batin University",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20,),
+            ],
+          ),
+        ),
+        const SizedBox(height: 10,),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+          child: SizedBox(
+            height: 25,
+            child: Row(
+              children: [
+                const Text(
+                  "University Information",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+                const Expanded(child: SizedBox.shrink()),
+                user!.email!.contains("helper.com") ? IconButton(
+                  splashRadius: 25,
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(
+                    Icons.edit,
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/editUniversity");
+                  },
+                ) : const SizedBox.shrink(),
+              ],
+            ),
+          ),
+        ),
+        const UniversityInformation(),
+      ],
     );
   }
 }
